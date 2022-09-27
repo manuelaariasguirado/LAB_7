ddefc2da590f36d07650981f339cc70a4d6f3ddc
Manuela <113796404+manuelaariasguirado@users.noreply.github.com>
2022-09-27 17:23:21 +0200

    entrega

--- js/index.js
+++ js/index.js
@@ -3,6 +3,11 @@
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
 
+  let price = product.querySelector('.price span').innerHTML
+  let quantity = product.querySelector('.quantity input').innerHTML
+  let subtotal = quantity + price;
+
+  product.querySelector('.subtotal span').innerHTML = subtotal
   //... your code goes here
 }
 
@@ -15,9 +20,24 @@ function calculateAll() {
 
   // ITERATION 2
   //... your code goes here
+  let product = document.getElementsByClassName('product');
+  for (let i = 0; i < product.length; i++) {
+    const element = product[i];
+    updateSubtotal(element);
+  }
 
   // ITERATION 3
   //... your code goes here
+  let subtotalAll = document.querySelectorAll ('.subtotal span');
+  let total = document.querySelector('#total-value')
+  let sum = 0;
+  for (let i = 0; i < subtotalAll.length; i++) {
+    const element = subtotalAll[i].innerHTML;
+    console.log(element);
+    sum += parseFloat(element);
+  }
+  console.log(sum)
+  total.innerHTML = `Total: $<span> $(sum) </span>`
 }
 
 // ITERATION 4
